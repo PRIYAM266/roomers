eb024c63a213be49e7c64d8f645cd9f902b05079
PRIYAM <priyam266german@gmail.com>
2021-08-26 20:37:43 +0530

    changed bottom nav bar

--- lib/screens/bottom_nav_bar/bottom_nav_bar_screen.dart
+++ lib/screens/bottom_nav_bar/bottom_nav_bar_screen.dart
@@ -1,10 +1,70 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
 import 'package:roomers/components/constants.dart';
 import 'package:roomers/screens/favorites/favorites_screen.dart';
 import 'package:roomers/screens/home/home_screen.dart';
 import 'package:roomers/screens/post_ad/post_ad_screen.dart';
 import 'package:roomers/screens/profile/profile_screen.dart';
 import 'package:roomers/screens/search/search_screen.dart';
+
+// class BottomNavBar extends StatefulWidget {
+//   const BottomNavBar({Key? key}) : super(key: key);
+//   static String idScreen = "/BottomNavBar";
+//
+//   @override
+//   _BottomNavBarState createState() => _BottomNavBarState();
+// }
+//
+// class _BottomNavBarState extends State<BottomNavBar> {
+//   int _currentIndex = 0;
+//   final List<Widget> _children = [
+//     HomeScreen(),
+//     SearchScreen(),
+//     PostAdScreen(),
+//     FavoritesScreen(),
+//     ProfileScreen(),
+//   ];
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: _children[_currentIndex],
+//       bottomNavigationBar: BottomNavigationBar(
+//         currentIndex: _currentIndex,
+//         type: BottomNavigationBarType.fixed,
+//         selectedItemColor: kPrimaryColor,
+//         backgroundColor: Colors.white,
+//         onTap: (index) {
+//           setState(() {
+//             _currentIndex = index;
+//           });
+//         },
+//         items: [
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.home),
+//             label: "Home",
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.search),
+//             label: "Search",
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.add),
+//             label: "Post",
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.favorite_border),
+//             label: "Favorites",
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.person),
+//             label: "Profile",
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
 
 class BottomNavBar extends StatefulWidget {
   const BottomNavBar({Key? key}) : super(key: key);
@@ -15,51 +75,92 @@ class BottomNavBar extends StatefulWidget {
 }
 
 class _BottomNavBarState extends State<BottomNavBar> {
-  int _currentIndex = 0;
-  final List<Widget> _children = [
-    HomeScreen(),
-    SearchScreen(),
-    PostAdScreen(),
-    FavoritesScreen(),
-    ProfileScreen(),
-  ];
+  PersistentTabController _controller =
+      PersistentTabController(initialIndex: 0);
+
+  List<Widget> _buildScreens() {
+    return [
+      HomeScreen(),
+      SearchScreen(),
+      PostAdScreen(),
+      FavoritesScreen(),
+      ProfileScreen(),
+    ];
+  }
+
+  List<PersistentBottomNavBarItem> _navBarsItems() {
+    return [
+      PersistentBottomNavBarItem(
+        icon: Icon(Icons.home),
+        title: ("Home"),
+        activeColorPrimary: kPrimaryColor,
+        inactiveColorPrimary: CupertinoColors.systemGrey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: Icon(Icons.search),
+        title: ("Search"),
+        activeColorPrimary: kPrimaryColor,
+        inactiveColorPrimary: CupertinoColors.systemGrey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+        title: ("Post"),
+        activeColorPrimary: kPrimaryColor,
+        inactiveColorPrimary: CupertinoColors.systemGrey,
+        iconSize: 30.0,
+      ),
+      PersistentBottomNavBarItem(
+        icon: Icon(Icons.favorite_border),
+        title: ("Favorites"),
+        activeColorPrimary: kPrimaryColor,
+        inactiveColorPrimary: CupertinoColors.systemGrey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: Icon(Icons.person),
+        title: ("Profile"),
+        activeColorPrimary: kPrimaryColor,
+        inactiveColorPrimary: CupertinoColors.systemGrey,
+      ),
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _children[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        type: BottomNavigationBarType.fixed,
-        selectedItemColor: kPrimaryColor,
-        backgroundColor: Colors.white,
-        onTap: (index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: "Home",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: "Search",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add),
-            label: "Post",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.favorite_border),
-            label: "Favorites",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: "Profile",
-          ),
-        ],
+    return PersistentTabView(
+      context,
+      controller: _controller,
+      screens: _buildScreens(),
+      items: _navBarsItems(),
+      confineInSafeArea: true,
+      backgroundColor: Colors.white, // Default is Colors.white.
+      handleAndroidBackButtonPress: true, // Default is true.
+      resizeToAvoidBottomInset:
+          true, // This needs to be true if you want to move up the screen when keyboard appears. Default is true.
+      stateManagement: true, // Default is true.
+      hideNavigationBarWhenKeyboardShows:
+          true, // Recommended to set 'resizeToAvoidBottomInset' as true while using this argument. Default is true.
+      decoration: NavBarDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        colorBehindNavBar: Colors.white,
       ),
+      popAllScreensOnTapOfSelectedTab: true,
+      popActionScreens: PopActionScreensType.all,
+      itemAnimationProperties: ItemAnimationProperties(
+        // Navigation Bar's items animation properties.
+        duration: Duration(milliseconds: 200),
+        curve: Curves.ease,
+      ),
+      screenTransitionAnimation: ScreenTransitionAnimation(
+        // Screen transition animation on change of selected tab.
+        animateTabTransition: true,
+        curve: Curves.ease,
+        duration: Duration(milliseconds: 200),
+      ),
+      navBarStyle:
+          NavBarStyle.style15, // Choose the nav bar style with this property.
     );
   }
 }
--- lib/screens/post_ad/components/body.dart
+++ lib/screens/post_ad/components/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:roomers/components/inputTextFormField.dart';
 
 class Body extends StatefulWidget {
   const Body({Key? key}) : super(key: key);
@@ -11,8 +12,63 @@ class _BodyState extends State<Body> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Center(
-        child: Text("Post Ad screen"),
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              Text(
+                "ROOMMATE POST",
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Container(
+                child: Column(
+                  children: [
+                    InputFormField(
+                      labelText: "Name",
+                      hintText: "Enter Name",
+                      textInputType: TextInputType.name,
+                      obscureText: false,
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    InputFormField(
+                      labelText: "State",
+                      hintText: "Enter State",
+                      textInputType: TextInputType.name,
+                      obscureText: false,
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    InputFormField(
+                      labelText: "Gender",
+                      hintText: "Enter Gender",
+                      textInputType: TextInputType.name,
+                      obscureText: false,
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    InputFormField(
+                      labelText: "Mother tongue",
+                      hintText: "Enter Mother tongue",
+                      textInputType: TextInputType.name,
+                      obscureText: false,
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
